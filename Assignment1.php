7a8f365b4e9dfecb0f9557f4b2efd7089c62dc82
Manuel Trasmonte <73626065+sensurozu@users.noreply.github.com>
2021-08-02 17:09:46 +0800

    Add files via upload

---
+++ Assignment1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>
+    Meh Information
+    </title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+       
+       body{
+           background-color: #9DC183;
+           font-size: 30px;
+           line-height: 50px;
+           font-family: 'Poppins';
+           color: #fff4f3;
+       }
+    </style>
+</head>
+<body>
+    <div class="main">
+        <?php
+          echo "Address: Blk 182 Lot 18 Tarhata St. Pembo, Makati City <br>
+                Age: 20 <br>
+                Favorite Color: Pale Green <br>
+                Favorite Food: Shawarma <br>
+                Religion: Catholic <br>
+                Birth Year: April 5, 2001 <br>
+                School: University of Makati";
+            ?>
+    </div>
+    
+</body>
+</html>
